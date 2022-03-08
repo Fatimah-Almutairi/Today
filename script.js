7b601e57fbbb30a01cf871405bb38ec79da16931
fatimah06 <fatima.m.mutairi@gmail.com>
2022-03-08 09:01:55 +0300

    1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const timeEl = document.getElementById('time');
 const dateEl = document.getElementById('date');
-
+const weatherEl = document.getElementsByClassName('weather-loading');
 const days =['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const monthes = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
@@ -19,56 +19,51 @@ setInterval(() => {
     dateEl.innerHTML = days[day] + ', '+ date+ ' '+monthes[month]
 },100);
 
-let weather = {
-    API_KEY: "4e12d4da7a57eafc6aac3e1417d041c0",
-    fetchWeather: function (city) {
-      fetch(
-        "https://api.openweathermap.org/data/2.5/weather?q=" +
-          city +
-          "&units=metric&appid=" +
-          this.API_KEY
-      )
-        .then((response) => {
-        //   if (!response.ok) {
-        //     alert("No weather found.");
-        //     throw new Error("No weather found.");
-        //   }
-          return response.json();
-        })
-        .then((data) => this.displayWeather(data));
-    },
-    displayWeather: function (data) {
-      const { name } = data;
-      const { icon, description } = data.weather[0];
-      const { temp, humidity } = data.main;
-      const { speed } = data.wind;
-      document.querySelector(".city").innerText = "Weather in " + name;
-      document.querySelector(".icon").src =
-        "https://openweathermap.org/img/wn/" + icon + ".png";
-      document.querySelector(".description").innerText = description;
-      document.querySelector(".temp").innerText = temp + "°C";
-      document.querySelector(".humidity").innerText =
-        "Humidity: " + humidity + "%";
-      document.querySelector(".wind").innerText =
-        "Wind speed: " + speed + " km/h";
-      document.querySelector(".weather").classList.remove("loading");
 
-    },
-    search: function () {
-      this.fetchWeather(document.querySelector(".search-bar").value);
-    },
-  };
+const API_KEY = '4e12d4da7a57eafc6aac3e1417d041c0';
+
+function fetchWeather(city){
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q="+
+      city+
+      "&units=metric&appid=" +
+      this.API_KEY
+  ).then(res => res.json()).then(data => this.displayWeather(data));
+  
+}
+function displayWeather(data){
+  const { name } = data;
+  const { icon, description } = data.weather[0];
+  const { temp, humidity } = data.main;
+  const { speed } = data.wind;
+  document.querySelector(".city").innerText = "Weather in " + name;
+  document.querySelector(".icon").src =
+    "https://openweathermap.org/img/wn/" + icon + ".png";
+  document.querySelector(".description").innerText = description;
+  document.querySelector(".temp").innerText = temp + "°C";
+  document.querySelector(".humidity").innerText =
+    "Humidity: " + humidity + "%";
+  document.querySelector(".wind").innerText =
+    "Wind speed: " + speed + " km/h";
+
+  function search_btn(){
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  }
+}
+
+fetchWeather()
+
   
   document.querySelector(".btn").addEventListener("click", function () {
-    weather.search();
+    search_btn();
   });
   
   document
     .querySelector(".search-bar")
     .addEventListener("keyup", function (event) {
       if (event.key == "Enter") {
-        weather.search();
+        search_btn();
       }
     });
   
-  weather.fetchWeather("Riyadh");
+  fetchWeather("Riyadh");
